d47d436862f9114cf364bd0fe844dfc69b8d83e5
lb <lb175666@163.com>
2023-09-25 22:12:27 +0800

    git仓库的第一个测试用例

--- chapter08/src/test/Test1.java
+++ chapter08/src/test/Test1.java
@@ -9,7 +9,7 @@ package test;
  * @Create 2023/9/25 21:34
  * @Version 1.0
  */
-public class Test {
+public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是我的仓库里的第一个输出");
     }
